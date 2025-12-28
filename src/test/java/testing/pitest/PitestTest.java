f6a0d7bd5ec6329e1d0eb2e3ac616ee2962105b1
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-12-28 23:52:10 +0100

    .

--- src/test/java/testing/pitest/PitestTest.java
+++ src/test/java/testing/pitest/PitestTest.java
@@ -29,3 +29,19 @@ class PitestTest {
         assertThat(calc.isPositive(10)).isTrue();
     }
 }
+
+class Calculator {
+    /**
+     * Checks if a number is positive (including zero).
+     */
+    public boolean isPositive(int number) {
+        // Mutation Opportunity:
+        // Pitest will try to change ">=" to ">" (Conditional Boundary Mutator).
+        // If our test only checks "10", it will pass both conditions.
+        // So the mutation will SURVIVE (which is bad/vulnerable).
+        if (number >= 0) {
+            return true;
+        }
+        return false;
+    }
+}
